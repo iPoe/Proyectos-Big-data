9f4a68d307f8f1c05acb0841ab13b057fa4105f7
iPoe <leonardosawi12@gmail.com>
2020-10-03 16:57:06 -0500

    lr

--- Proyecto1/proyecto3.py
+++ Proyecto1/proyecto3.py
@@ -102,7 +102,7 @@ data=assembler.transform(data)
 
 train, test = data.randomSplit([0.8, 0.2], seed=12345)
 
-lr = LogisticRegression(labelCol="AuthorNum", featuresCol="features",maxIter=10,regParam=0.1, elasticNetParam=1.0, family="multinomial")
+lr = LogisticRegression(labelCol="AuthorNum", featuresCol="features",maxIter=10,regParam=0.1, family="multinomial")
 
 # Fit the model
 lrModel = lr.fit(train)
