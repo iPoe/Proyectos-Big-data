d6d95e2092235c83e5b7a7bdd56c01985601b5ec
root <root@sandbox-hdp.hortonworks.com>
2020-10-03 20:07:21 +0000

    Dataset limpio

---
+++ Proyecto1/proyecto3.py
@@ -0,0 +1,117 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import isnan, when, count, col, explode, array, lit
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.feature import Imputer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import StandardScaler
+from pyspark.ml.feature import ChiSqSelector
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+import matplotlib.pyplot as plt
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.sql.functions import dayofweek
+from pyspark.sql.functions import isnan, when, count, col, lit, sum
+from pyspark.sql.functions import (to_date, datediff, date_format,month)
+
+#Se carga el conjunto de datos
+spark = SparkSession.builder.master("local").appName("Avila").config("spark.some.config.option","some-value").getOrCreate()
+data = spark.read.format("csv").option("header","true").option("inferSchema", "true").load(r"file:///home/maria_dev/avila.csv")
+
+#####################################################################################################
+#PRIMER PUNTO
+#DESCRIPCION DEL CONJUNTO DE DATOS INICIAL
+#Imprime la cantidad de registros y atributos respectivamente
+
+print("Registros:",data.count(),", Atributos:",len(data.columns))
+
+#Tipo de los atributos
+print(data.printSchema())
+
+# Se revisa si existen nulos en alguno de los atributos del dataset
+print(data.select([count(when(isnan(c),c)).alias(c) for c in data.columns]).toPandas().head())
+
+#Descripcion de los atributos
+print(data.describe().select("Summary","F1","F2","F3","F4","F5").show())
+print(data.describe().select("Summary","F6","F7","F8","F9","F10").show())
+
+#Se verifica la correlacion entre los atributos
+pd = data.toPandas()
+print(pd.corr())
+
+#Diagrama de cajas para verificar datos atipicos
+plt.boxplot((pd['F1'],pd['F2'],pd['F3'],pd['F4'],pd['F5'],pd['F6'],pd['F7'],pd['F8'],pd['F9'],pd['F10']))
+plt.show()
+
+#Distribucion del atributo clasificador
+data.groupby("Author").count().show()
+
+
+#####################################################################################################
+#COMIENZA EL SEGUNDO PUNTO
+#LIMPIEZA DE LOS DATOS
+
+#Como se puede ver en los diagramas de cajas, el atributo F2 tiene datos que son demasiado atipicos
+#Estos registros se eliminaran
+print("LIMPIEZA DE LOS DATOS")
+data = data.filter(data.F2<350)
+print("Datos Atipicos F2 Eliminados:",data.count())
+
+#Se elimina el atributo F10
+data = data.drop('F10')
+print("Atributo F10 Eliminado:",data.columns)
+
+#Convertir los atributos categoricos a numericos
+indexer = StringIndexer(inputCol="Author", outputCol="AuthorNum")
+data = indexer.fit(data).transform(data)
+data = data.drop('Author')
+data.groupby("AuthorNum").count().show()
+
+#Se balancea cada categoria
+A = data.filter(data.AuthorNum == 0.0).sample(fraction=0.9)
+#A.groupby("AuthorNum").count().show()
+F = data.filter(col("AuthorNum") == 1.0).withColumn("dummy", explode(array([lit(x) for x in range(2)]))).drop('dummy')
+E = data.filter(col("AuthorNum") == 2.0).withColumn("dummy", explode(array([lit(x) for x in range(3)]))).drop('dummy')
+I = data.filter(col("AuthorNum") == 3.0).withColumn("dummy", explode(array([lit(x) for x in range(3)]))).drop('dummy')
+X = data.filter(col("AuthorNum") == 4.0).withColumn("dummy", explode(array([lit(x) for x in range(5)]))).drop('dummy')
+H = data.filter(col("AuthorNum") == 5.0).withColumn("dummy", explode(array([lit(x) for x in range(5)]))).drop('dummy')
+G = data.filter(col("AuthorNum") == 6.0).withColumn("dummy", explode(array([lit(x) for x in range(6)]))).drop('dummy')
+D = data.filter(col("AuthorNum") == 7.0).withColumn("dummy", explode(array([lit(x) for x in range(7)]))).drop('dummy')
+Y = data.filter(col("AuthorNum") == 8.0).withColumn("dummy", explode(array([lit(x) for x in range(10)]))).drop('dummy')
+C = data.filter(col("AuthorNum") == 9.0).withColumn("dummy", explode(array([lit(x) for x in range(25)]))).drop('dummy')
+W = data.filter(col("AuthorNum") == 10.0).withColumn("dummy", explode(array([lit(x) for x in range(57)]))).drop('dummy')
+B = data.filter(col("AuthorNum") == 11.0).withColumn("dummy", explode(array([lit(x) for x in range(500)]))).drop('dummy')
+
+#Se juntan todas las categorias balanceadas
+data = A.union(B).union(C).union(D).union(E).union(F).union(G).union(H).union(I).union(W).union(Y).union(X)
+data.groupby("AuthorNum").count().show()
+print("Numero de Registros Dataset Limpio:",data.count(),", Atributos:",len(data.columns))
+
+#####################################################################################################
+#COMIENZA PUNTO 3
+"""
+cols=data.columns
+cols.remove("AuthorNum")
+# Let us import the vector assembler
+
+assembler = VectorAssembler(inputCols=cols,outputCol="features")
+# Now let us use the transform method to transform our dataset
+data=assembler.transform(data)
+data.select("features").show(truncate=False)
+
+train, test = data.randomSplit([0.8, 0.2], seed=12345)
+
+lr = LogisticRegression(labelCol="AuthorNum", featuresCol="features",maxIter=10)
+
+# Fit the model
+lrModel = lr.fit(train)
+
+# Print the coefficients and intercept for multinomial logistic regression
+print("Coefficients:" + str(lrModel.coefficientMatrix))
+
+predict_train=lrModel.transform(train)
+predict_test=lrModel.transform(test)
+
+evaluator = MulticlassClassificationEvaluator(labelCol="AuthorNum", predictionCol="prediction", metricName="accuracy")
+lr_accuracy = evaluator.evaluate(predict_test)
+print("Accuracy of LogisticRegression is = %g"% (lr_accuracy))"""
+
