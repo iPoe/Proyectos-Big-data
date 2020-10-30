e3daaf3ccfb2e03758592faaa8e8797c1d9d71d3
iPoe <leonardosawi12@gmail.com>
2020-10-29 23:29:42 -0500

    Update test_streaming

--- Proyecto2/test_streaming.py
+++ Proyecto2/test_streaming.py
@@ -1,4 +1,9 @@
+from pyspark.sql import SparkSession
+
 from pyspark.sql.types import TimestampType, StringType, StructType, StructField
+
+
+spark = SparkSession.builder.master("local").appName("Avila").config("spark.some.config.option","some-value").getOrCreate()
 
 
 
